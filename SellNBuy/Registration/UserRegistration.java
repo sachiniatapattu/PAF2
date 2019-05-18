8683d0650b72a2ad01be0a00c4680d9e5d9fe84c
nimal <nimal@DESKTOP-CE2E60F>
2019-05-18 19:26:38 +0530

    Nimali

---
+++ SellNBuy/Registration/UserRegistration.java
@@ -0,0 +1,58 @@
+package SellNBuy;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+public class UserRegistration {
+	
+
+	@Entity
+	@Table(name="Users")
+	@EntityListeners(AuditingEntityListener.class)
+	public class Employee implements Serializable{
+	private static final long serialVersionUID = 1143993377101043360L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String designation;
+	private String expertise;
+	public String getName() {
+	return name;
+	}
+	public void setName(String name) {
+	this.name = name;
+	}
+	public String getDesignation() {
+	return designation;
+	}
+	public void setDesignation(String designation) {
+	this.designation = designation;
+	}
+	public String getExpertise() {
+	return expertise;
+	}
+	public void setExpertise(String expertise) {
+	this.expertise = expertise;
+	}
+	public Long getId() {
+	return id;
+	}
+	public void setId(Long id) {
+	this.id = id;
+	}
+	}
+}
